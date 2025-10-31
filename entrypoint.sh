348a1584f328e3fdfadf7b263416698c8a1e7f97
Santi <chesslike693@gmail.com>
2025-10-31 16:08:13 -0300

    remove PostgreSQL wait logic from entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,6 @@
 
 set -e
 
-echo "Waiting for PostgreSQL..."
-while ! nc -z db 5432; do
-  sleep 0.1
-done
-echo "PostgreSQL started"
-
 echo "Running migrations..."
 python manage.py migrate --noinput
 
